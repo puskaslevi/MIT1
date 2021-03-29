49f7d401079dd2ff9f9fc478d36329d6c2599419
Puskás Levente <puskas.levente99@gmail.com>
2021-03-29 18:04:33 +0200

    Implemented alarm for speed limit change

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -24,6 +24,8 @@ public class TrainSensorImpl implements TrainSensor {
 	public void overrideSpeedLimit(int speedLimit) {
 		this.speedLimit = speedLimit;
 		controller.setSpeedLimit(speedLimit);
+		if (speedLimit < 0 || speedLimit > 500 || speedLimit < controller.getReferenceSpeed()*0.5)
+			user.setAlarmState(true);
 	}
 
 }
